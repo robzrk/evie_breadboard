3d3ce0200092d088ddcf4ab175af59b197dec544
Rob Zirkel <robzirkel@gmail.com>
2025-04-05 16:27:44 -0500

    added background color

--- src/shift_register_project.c
+++ src/shift_register_project.c
@@ -5,7 +5,7 @@
 #include <pigpio.h>
 
 //globals
-const int display_usec = 75000;
+const int display_usec = 20000;
 const float period = 1;
 
 #define SRCLK_GPIO 17
@@ -76,7 +76,7 @@ void clear(void) {
 }
 
 void display_bitmap(unsigned int bitmap) {
-  //printf("bitmap: 0x%x\n", bitmap);
+  //printf("bitmap: 0x%06x\n", bitmap);
   led_on(OE_GPIO); //output disabled	
   for (int j=0; j<25; ++j) {
     if (bitmap & (1 << j)) {
@@ -112,40 +112,107 @@ void simple_chaser(void) {
 }
 
 void chase_back() {
-  int last_color = 1;
-  int color = 2;
+  int last_fg_color = 1;
+  int fg_color = 2;
+  int last_bg_color = 3;
+  int bg_color = 4;
   int every_other = 0;
   unsigned int bitmap_list[24];
-  unsigned int bitmap;
+  unsigned int fg_bitmap;
+  unsigned int bg_bitmap;
   int k;
+  int max_fg_itr = 4;
+  int max_bg_itr = max_fg_itr * 8;
+  int bg_itr = 0;
+  int fg_itr = 0;
+  unsigned int mask;
+  int bail_cnt = 0;
   while (1) {
-    bitmap = 0;
-    while (color == last_color || color == 0) {
-      color = rand()%8;
+    fg_bitmap = 0;
+    bg_bitmap = 0;
+      
+    if (bg_itr++ >= max_bg_itr) {
+      while (bg_color == last_bg_color) {
+	bg_color = rand()%8;
+      }
+      bg_itr = 0;
     }
-    if (color & 0x1)
-      bitmap |= 0x10000;
-    if (color & 0x2)
-      bitmap |= 0x100;
-    if (color & 0x4)
-      bitmap |= 0x1;
+    if (fg_itr++ >= max_fg_itr) {
+      bail_cnt = 0;
+      while ((fg_color == last_fg_color || (fg_color & bg_color) == fg_color)
+	     && (bail_cnt++ < 1000)) {
+	fg_color = rand()%8;
+      }
+      fg_itr = 0;
+    }
+    if (fg_color & 0x1) // set R component
+      fg_bitmap |= 0x10000;
+    if (fg_color & 0x2) // set G component
+      fg_bitmap |= 0x100;
+    if (fg_color & 0x4) // set B component
+      fg_bitmap |= 0x1;
+
+    if (bg_color & 0x1) // set R component
+      bg_bitmap |= 0xff0000;
+    if (bg_color & 0x2) // set G component
+      bg_bitmap |= 0xff00;
+    if (bg_color & 0x4) // set B component
+      bg_bitmap |= 0xff;
+    
     for (int i=0; i<8; ++i) {
       if (every_other)
 	k = 7-i;
       else
 	k = i;
-      bitmap_list[k] = bitmap<<i;
+      // 111110
+      // 111101
+      // 111011
+      // 110111
+      // 101111
+      // 011111
+      switch (i) {
+      case 0:
+	mask = 0xfefefe;
+	break;
+      case 1:
+	mask = 0xfdfdfd;
+	break;
+      case 2:
+	mask = 0xfbfbfb;
+	break;
+      case 3:
+	mask = 0xf7f7f7;
+	break;
+      case 4:
+	mask = 0xefefef;
+	break;
+      case 5:
+	mask = 0xdfdfdf;
+	break;
+      case 6:
+	mask = 0xbfbfbf;
+	break;
+      case 7:
+      default:
+	mask = 0x7f7f7f;
+	break;
+      }
+      
+      bitmap_list[k] = (bg_bitmap & mask) | fg_bitmap<<i;
+      //bitmap_list[k] = bg_bitmap | (fg_bitmap<<i);
     }
+    //printf("fg: 0x%x bg: 0x%x\n", fg_color, bg_color);
+    //printf("fg_bitmap: 0x%06x, bg_bitmap: 0x%06x\n", fg_bitmap, bg_bitmap);
     for (int i=0; i<8; ++i) {
-      bitmap = bitmap_list[i];
-      display_bitmap(bitmap);
+      display_bitmap(bitmap_list[i]);
       clear();
     }
     if (every_other == 0)
       every_other = 1;
     else
       every_other = 0;
-    last_color = color;
+    last_fg_color = fg_color;
+    last_bg_color = bg_color;
   }
 }
 
@@ -162,8 +229,8 @@ void test_fn(void) {
 int main(void) {
   printf("Dispay Usec: %d, Period: %f\n", display_usec, period);
   gpio_setup();
-  simple_chaser();
-  //chase_back();
+  //simple_chaser();
+  chase_back();
   //test_fn();
   gpioTerminate();
   return 0;
